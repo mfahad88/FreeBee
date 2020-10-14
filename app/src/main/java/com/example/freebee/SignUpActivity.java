6ea78e965e54183ffc9029ecf59a710eeaa8f552
Fahad <mfahad88@outlook.com>
2020-10-14 08:32:16 +0500

    1

--- app/src/main/java/com/example/freebee/SignUpActivity.java
+++ app/src/main/java/com/example/freebee/SignUpActivity.java
@@ -1,46 +1,75 @@
 package com.example.freebee;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.emoji.bundled.BundledEmojiCompatConfig;
+import androidx.emoji.text.EmojiCompat;
+import androidx.emoji.widget.EmojiTextView;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.ahmadrosid.svgloader.SvgLoader;
+
 import com.example.freebee.helper.Utils;
 import com.example.freebee.models.country.Country;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.juanpabloprado.countrypicker.CountryPicker;
+import com.juanpabloprado.countrypicker.CountryPickerListener;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class SignUpActivity extends AppCompatActivity {
-    Dialog dialog;
-    ListView listView;
+
+
     Gson gson;
     List<Country> countries;
+    EmojiTextView txtView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-        dialog=new Dialog(this);
-        gson=new Gson();
 
-        countries = gson.fromJson(Utils.loadFromAsset(this,"country.json"), new TypeToken<List<Country>>(){}.getType());
-        dialog.setContentView(R.layout.dialog_country);
-        listView=dialog.findViewById(R.id.listView);
+
+        setContentView(R.layout.activity_sign_up);
+        countries=new ArrayList<>();
+
+        for(Locale locale:Locale.getAvailableLocales()){
+            if(!TextUtils.isEmpty(locale.getDisplayCountry())) {
+                countries.add(new Country(PhoneNumberUtil.getInstance().getCountryCodeForRegion(locale.getCountry()), locale.getDisplayCountry(), countryCodeToEmoji(locale.getCountry())));
+                Log.e("LOcale--->", locale.getCountry()+", "+locale.getDisplayCountry()+ ", "+PhoneNumberUtil.getInstance().getCountryCodeForRegion(locale.getCountry()));
+            }
+
+
+        }
+
+        final Dialog dialog=new Dialog(this);
+        ListView listView=new ListView(this);
+        dialog.addContentView(listView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         listView.setAdapter(new BaseAdapter() {
             @Override
             public int getCount() {
@@ -54,31 +83,64 @@ public class SignUpActivity extends AppCompatActivity {
 
             @Override
             public long getItemId(int i) {
-                return i;
+                return countries.get(i).getCountryCode();
             }
 
             @Override
             public View getView(int i, View view, ViewGroup viewGroup) {
-                if(view==null){
-                    view= LayoutInflater.from(SignUpActivity.this).inflate(R.layout.list_countries_row,viewGroup,false);
-                }
                 final Country country=countries.get(i);
-                ImageView imageView_flag=view.findViewById(R.id.imageView_flag);
+                if(view==null){
+                    EmojiCompat.Config config = new BundledEmojiCompatConfig(SignUpActivity.this);
+                    EmojiCompat.init(config);
+                    view=LayoutInflater.from(SignUpActivity.this).inflate(R.layout.list_countries_row,viewGroup,false);
+                }
+
+                EmojiTextView txtView=view.findViewById(R.id.txtView);
                 TextView textView_country=view.findViewById(R.id.textView_country);
-                SvgLoader.pluck().with(SignUpActivity.this).load(country.getFlag(),imageView_flag);
-                textView_country.setText(country.getName());
+                textView_country.setText(country.getCountryName());
+                txtView.setText(country.getCountryFlag());
                 view.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Toast.makeText(SignUpActivity.this, ""+country.getFlag(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SignUpActivity.this, ""+country.toString(), Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
                     }
                 });
                 return view;
             }
         });
+
         dialog.show();
-        dialog.setCancelable(false);
-        dialog.setCanceledOnTouchOutside(false);
+
+   }
+
+    public String countryCodeToEmoji(String code) {
+
+        // offset between uppercase ascii and regional indicator symbols
+        int OFFSET = 127397;
+
+        // validate code
+        if (code == null || code.length() != 2) {
+            return "";
+        }
+
+        //fix for uk -> gb
+        if (code.equalsIgnoreCase("uk")) {
+            code = "gb";
+        }
+
+        // convert code to uppercase
+        code = code.toUpperCase();
+
+        StringBuilder emojiStr = new StringBuilder();
+
+        //loop all characters
+        for (int i = 0; i < code.length(); i++) {
+            emojiStr.appendCodePoint(code.charAt(i) + OFFSET);
+        }
+
+        // return emoji
+        return emojiStr.toString();
     }
 
 }
--- app/src/main/java/com/example/freebee/models/country/Country.java
+++ app/src/main/java/com/example/freebee/models/country/Country.java
@@ -6,250 +6,46 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Country {
+    int countryCode;
+    String countryName;
+    String countryFlag;
 
-    @SerializedName("name")
-    @Expose
-    private String name;
-    @SerializedName("topLevelDomain")
-    @Expose
-    private List<String> topLevelDomain = null;
-    @SerializedName("alpha2Code")
-    @Expose
-    private String alpha2Code;
-    @SerializedName("alpha3Code")
-    @Expose
-    private String alpha3Code;
-    @SerializedName("callingCodes")
-    @Expose
-    private List<String> callingCodes = null;
-    @SerializedName("capital")
-    @Expose
-    private String capital;
-    @SerializedName("altSpellings")
-    @Expose
-    private List<String> altSpellings = null;
-    @SerializedName("region")
-    @Expose
-    private String region;
-    @SerializedName("subregion")
-    @Expose
-    private String subregion;
-    @SerializedName("population")
-    @Expose
-    private String population;
-    @SerializedName("latlng")
-    @Expose
-    private List<String> latlng = null;
-    @SerializedName("demonym")
-    @Expose
-    private String demonym;
-    @SerializedName("area")
-    @Expose
-    private String area;
-    @SerializedName("gini")
-    @Expose
-    private Object gini;
-    @SerializedName("timezones")
-    @Expose
-    private List<String> timezones = null;
-    @SerializedName("borders")
-    @Expose
-    private List<String> borders = null;
-    @SerializedName("nativeName")
-    @Expose
-    private String nativeName;
-    @SerializedName("numericCode")
-    @Expose
-    private String numericCode;
-    @SerializedName("currencies")
-    @Expose
-    private List<Currency> currencies = null;
-    @SerializedName("languages")
-    @Expose
-    private List<Language> languages = null;
-    @SerializedName("translations")
-    @Expose
-    private Translations translations;
-    @SerializedName("flag")
-    @Expose
-    private String flag;
-    @SerializedName("regionalBlocs")
-    @Expose
-    private List<RegionalBloc> regionalBlocs = null;
-
-    public String getName() {
-        return name;
+    public Country(int countryCode, String countryName, String countryFlag) {
+        this.countryCode = countryCode;
+        this.countryName = countryName;
+        this.countryFlag = countryFlag;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public int getCountryCode() {
+        return countryCode;
     }
 
-    public List<String> getTopLevelDomain() {
-        return topLevelDomain;
+    public void setCountryCode(int countryCode) {
+        this.countryCode = countryCode;
     }
 
-    public void setTopLevelDomain(List<String> topLevelDomain) {
-        this.topLevelDomain = topLevelDomain;
+    public String getCountryName() {
+        return countryName;
     }
 
-    public String getAlpha2Code() {
-        return alpha2Code;
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
     }
 
-    public void setAlpha2Code(String alpha2Code) {
-        this.alpha2Code = alpha2Code;
+    public String getCountryFlag() {
+        return countryFlag;
     }
 
-    public String getAlpha3Code() {
-        return alpha3Code;
+    public void setCountryFlag(String countryFlag) {
+        this.countryFlag = countryFlag;
     }
 
-    public void setAlpha3Code(String alpha3Code) {
-        this.alpha3Code = alpha3Code;
-    }
-
-    public List<String> getCallingCodes() {
-        return callingCodes;
-    }
-
-    public void setCallingCodes(List<String> callingCodes) {
-        this.callingCodes = callingCodes;
-    }
-
-    public String getCapital() {
-        return capital;
-    }
-
-    public void setCapital(String capital) {
-        this.capital = capital;
-    }
-
-    public List<String> getAltSpellings() {
-        return altSpellings;
-    }
-
-    public void setAltSpellings(List<String> altSpellings) {
-        this.altSpellings = altSpellings;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public String getSubregion() {
-        return subregion;
-    }
-
-    public void setSubregion(String subregion) {
-        this.subregion = subregion;
-    }
-
-    public String getPopulation() {
-        return population;
-    }
-
-    public void setPopulation(String population) {
-        this.population = population;
-    }
-
-    public List<String> getLatlng() {
-        return latlng;
-    }
-
-    public void setLatlng(List<String> latlng) {
-        this.latlng = latlng;
-    }
-
-    public String getDemonym() {
-        return demonym;
-    }
-
-    public void setDemonym(String demonym) {
-        this.demonym = demonym;
-    }
-
-    public String getArea() {
-        return area;
-    }
-
-    public void setArea(String area) {
-        this.area = area;
-    }
-
-    public Object getGini() {
-        return gini;
-    }
-
-    public void setGini(Object gini) {
-        this.gini = gini;
-    }
-
-    public List<String> getTimezones() {
-        return timezones;
-    }
-
-    public void setTimezones(List<String> timezones) {
-        this.timezones = timezones;
-    }
-
-    public List<String> getBorders() {
-        return borders;
-    }
-
-    public void setBorders(List<String> borders) {
-        this.borders = borders;
-    }
-
-    public String getNativeName() {
-        return nativeName;
-    }
-
-    public void setNativeName(String nativeName) {
-        this.nativeName = nativeName;
-    }
-
-    public String getNumericCode() {
-        return numericCode;
-    }
-
-    public void setNumericCode(String numericCode) {
-        this.numericCode = numericCode;
-    }
-
-    public List<Currency> getCurrencies() {
-        return currencies;
-    }
-
-    public void setCurrencies(List<Currency> currencies) {
-        this.currencies = currencies;
-    }
-
-    public List<Language> getLanguages() {
-        return languages;
-    }
-
-    public void setLanguages(List<Language> languages) {
-        this.languages = languages;
-    }
-
-    public Translations getTranslations() {
-        return translations;
-    }
-
-    public void setTranslations(Translations translations) {
-        this.translations = translations;
-    }
-
-    public String getFlag() {
-        return flag;
-    }
-
-    public void setFlag(String flag) {
-        this.flag = flag;
+    @Override
+    public String toString() {
+        return "Country{" +
+                "countryCode=" + countryCode +
+                ", countryName='" + countryName + '\'' +
+                ", countryFlag='" + countryFlag + '\'' +
+                '}';
     }
 }
